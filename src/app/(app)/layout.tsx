013c1198ef2f0d6f6133f3d6599ad36a60860566
SHAREEF SHAIK <shareef.08.2005@gmail.com>
2025-10-13 21:44:21 +0530

    ready for deployment

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/Navbar";
-import "../globals.css"; // or "../../globals.css" depending on your folder depth
-
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+import "../globals.css";
 
 export const metadata: Metadata = {
   title: "App Dashboard",
